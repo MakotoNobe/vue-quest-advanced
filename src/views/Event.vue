1179c783082c751316b76b6ecb8a3dd7d8b95deb
MakotoNobe <astp.eep92@gmail.com>
2021-03-09 18:06:49 +0900

    event add

---
+++ src/views/Event.vue
@@ -0,0 +1,24 @@
+<template>
+ <div>
+<router-link to="/">ホームへ戻る</router-link>
+<router-link to="/event/1">イベント1</router-link>
+<router-link to="/event/2">イベント2</router-link>
+<router-link to="/event/3">イベント3</router-link>
+<h1>イベントナンバー{{ eventNO }}</h1>
+ </div>
+</template>
+
+
+<script>
+    export default{
+        name:"Event",
+        components:{},
+        computed:{
+            eventNO:{
+                get(){
+                    return this.$route.params["eventNO"]; 
+                }
+            }
+        }
+    }
+</script>
